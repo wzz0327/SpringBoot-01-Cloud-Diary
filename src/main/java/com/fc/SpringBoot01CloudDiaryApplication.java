bdfe69f435baeca7eff0b7c57508ed938ef750a6
wzz0327 <100327410+wzz0327@users.noreply.github.com>
2022-04-30 16:01:10 +0800

    Add files via upload
    基础项目创建

---
+++ src/main/java/com/fc/SpringBoot01CloudDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01CloudDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot01CloudDiaryApplication.class, args);
+    }
+
+}
